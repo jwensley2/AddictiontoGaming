670cefa5506d8024be3e720017c2796d6b7e2527
Joseph Wensley <jwensley2@gmail.com>
2013-04-04 00:30:13 -0400

    Default to empty array when reading packets

--- application/libraries/servers/source_rcon.php
+++ application/libraries/servers/source_rcon.php
@@ -1,7 +1,7 @@
 <?php
 DEFINE('PACKET_SIZE', '4096');
 DEFINE('SERVERDATA_AUTH', 03);
-DEFINE('SERVERDATA_EXECCOMMAND', 02); 
+DEFINE('SERVERDATA_EXECCOMMAND', 02);
 
 class Source_rcon
 {
@@ -9,103 +9,124 @@ class Source_rcon
 	private $request_id;
 	private $socket;
 	private $timeout = .5;
-	
+
 	// Protected Vars
 	protected $authenticated = false;
-	
+
 	function connect($ip, $port, $password)
-	{	
+	{
 		$this->request_id = 1;
-		
+
 		// Open socket to the server
 		$this->socket = fsockopen('tcp://'.$ip, $port, $errno, $errstr, 1);
 		socket_set_timeout($this->socket, $this->timeout);
-		
+
 		// Write the auth packet
 		$this->write(SERVERDATA_AUTH, $password);
 
-
 		$this->request_id++;
-		
+
 		// Read the response packet
 		$response = $this->read_packet();
-		
-		if($response[1]['response']){
-			$response = $this->read_packet();	
+
+		if ($response[1]['response'])
+		{
+			$response = $this->read_packet();
 		}
-		
-		if (isset($response[1]['id']) && $response[1]['id'] == -1){
+
+		if (isset($response[1]['id']) && $response[1]['id'] == -1)
+		{
 			$this->authenticated = false;
 			return false;
-		}else{
+		}
+		else
+		{
 			$this->authenticated = true;
 			return true;
 		}
 	}
-	
+
 	function send_command($command)
 	{
-		if(!$this->authenticated){
+		if ( ! $this->authenticated)
+		{
 			return "You must be authenticated with the server before sending commands!";
 		}
-		
+
 		// Build the commmand packet
 		$this->write(SERVERDATA_EXECCOMMAND, $command);
 		$data = pack("V", strlen($data)).$data;
-		
+
 		// Send the packet
 		fwrite($this->socket, $data, strlen($data));
-		
+
 		$this->request_id++;
-		
+
 		$response = $this->read();
-		
+
 		dump($response);
-		
+
 		return $response[1]['s1'];
 	}
-	
+
 	private function read()
 	{
 		$packets = $this->read_packet();
-		
-		foreach($packets as $pack){
-		    if (isset($return[$pack['id']])){
+
+		if ( ! $packets)
+		{
+			return;
+		}
+
+		foreach($packets as $pack)
+		{
+			if (isset($return[$pack['id']]))
+			{
 				$return[$pack['id']]['S1'] .= $pack['s1'];
 				$return[$pack['id']]['s2'] .= $pack['s1'];
-			}else{
+			}
+			else
+			{
 				$return[$pack['ID']] = array(
 					'response' => $pack['response'],
 					's1' => $pack['s1'],
-					's2' =>	$pack['s2'],
+					's2' => $pack['s2'],
 				);
 			}
 		}
 		return $return;
 	}
-	
+
 	private function read_packet()
 	{
 		$i = 1;
-		while($size = fread($this->socket, 4)){
+
+		$return = array();
+
+		while ($size = fread($this->socket, 4))
+		{
 			$size = $this->get_long($size);
-			
-			if($size > PACKET_SIZE){
-				$packet = "\x00\x00\x00\x00\x00\x00\x00\x00".fread($this->socket,PACKET_SIZE);
-			}else{
+
+			if ($size > PACKET_SIZE)
+			{
+				$packet = "\x00\x00\x00\x00\x00\x00\x00\x00".fread($this->socket, PACKET_SIZE);
+			}
+			else
+			{
 				$packet = fread($this->socket, $size);
 			}
-			
-			$return[$i]['id']		= $this->get_long($packet);
-			$return[$i]['reponse']	= $this->get_long($packet);
-			$return[$i]['s1']		= $this->get_string($packet);
-			$return[$i]['s2']		= $this->get_string($packet);
-			
+
+			$return[$i]['id']       = $this->get_long($packet);
+			$return[$i]['reponse']  = $this->get_long($packet);
+			$return[$i]['s1']       = $this->get_string($packet);
+			$return[$i]['s2']       = $this->get_string($packet);
+
 			$i++;
 		}
+
 		return $return;
 	}
-	
+
 	private function write($cmd, $s1='', $s2='')
 	{
 		// Build the auth packet and send it
@@ -113,7 +134,7 @@ class Source_rcon
 		$data = pack("V", strlen($data)).$data;
 		fwrite($this->socket, $data, strlen($data));
 	}
-	
+
 	private function get_byte(&$string)
 	{
 		$data = substr($string, 0, 1);
