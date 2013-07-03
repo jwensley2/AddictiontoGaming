582527255c0be62dde1ab98ce559fdc705459147
Joseph Wensley <jwensley2@gmail.com>
2013-07-02 20:52:53 -0400

    User Ardent and setup validation rules

--- app/models/User.php
+++ app/models/User.php
@@ -1,9 +1,10 @@
 <?php
 
+use LaravelBook\Ardent\Ardent;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableInterface;
 
-class User extends Eloquent implements UserInterface, RemindableInterface {
+class User extends Ardent implements UserInterface, RemindableInterface {
 
 	/**
 	 * The database table used by the model.
@@ -20,13 +21,40 @@ class User extends Eloquent implements UserInterface, RemindableInterface {
 	protected $hidden = array('password');
 
 	/**
+	 * Validation Rules
+	 */
+	public static $rules = array(
+		'username' => 'required|alpha_dash|between:3,50|unique:users,username',
+		'email'    => 'required|email|unique:users,email',
+		'password' => 'required|min:6'
+	);
+
+	// ------------------------------------------------------------------------
+
+	/**
+	 * Run before saving the data
+	 * @param  boolean $forced Is it a forced save?
+	 * @return bool
+	 */
+	public function beforeSave($forced = false)
+	{
+		// if there's a new password, hash it
+		if ($this->isDirty('password'))
+		{
+			$this->password = Hash::make($this->password);
+		}
+
+		return true;
+	}
+
+	/**
 	 * Get the unique identifier for the user.
 	 *
 	 * @return mixed
 	 */
 	public function getAuthIdentifier()
 	{
-		return $this->getKey();
+		return $this->username;
 	}
 
 	/**
