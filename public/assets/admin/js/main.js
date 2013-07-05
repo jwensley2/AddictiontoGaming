2620dfd9413466f9ba9e8a9841faf13d22730dd3
Joseph Wensley <jwensley2@gmail.com>
2013-07-05 18:38:04 -0400

    scroll to the top after clicking delete don't show multiple alerts for deleting the same post

--- public/assets/admin/js/main.js
+++ public/assets/admin/js/main.js
@@ -5,18 +5,29 @@ $(document).ready(function() {
 
 	$(".news-item .delete").on("click", function() {
 		var $btn    = $(this),
+			$alert  = $alertDeleteTmpl.clone().attr("id", null),
 			$row    = $btn.parents(".news-item").eq(0),
-			$alert  = $alertDeleteTmpl.clone(),
 			$keep   = $alert.find("[data-action=keep]"),
 			$delete = $alert.find("[data-action=delete]");
 
+		var id = $row.data("id");
+
+		$(window).scrollTop(0);
+
+		// If an alert already exists then we don't need to do anything
+		if ($("#delete-post-"+id).length > 0) return;
+
+		// Setup the alert
+		$alert.attr("id", "delete-post-"+id);
 		$alert.find(".title").text($row.data("title"));
 		$alert.insertBefore("#news-list").show().alert();
 
+		// Bind the keep button
 		$keep.on("click", function() {
 			$alert.alert("close");
 		});
 
+		// Bind the delete button
 		$delete.on("click", function() {
 			var url = $row.data("delete");
 
@@ -29,7 +40,6 @@ $(document).ready(function() {
 					var $errorAlert = $alertErrorTmpl.clone();
 
 					$errorAlert.append(result.message);
-
 					$errorAlert.insertBefore("#news-list").show();
 				}
 			}, "json");
