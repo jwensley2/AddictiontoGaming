0c30a598d7b609a39ba5857daca9101cafd9befc
Joseph Wensley <jwensley2@gmail.com>
2013-07-05 17:49:41 -0400

    Install profiler

--- app/config/app.php
+++ app/config/app.php
@@ -114,6 +114,7 @@ return array(
 		'Illuminate\View\ViewServiceProvider',
 		'Illuminate\Workbench\WorkbenchServiceProvider',
 		'Way\Generators\GeneratorsServiceProvider',
+		'Profiler\ProfilerServiceProvider',
 
 	),
 
@@ -179,6 +180,7 @@ return array(
 		'URL'             => 'Illuminate\Support\Facades\URL',
 		'Validator'       => 'Illuminate\Support\Facades\Validator',
 		'View'            => 'Illuminate\Support\Facades\View',
+		'Profiler'        => 'Profiler\Facades\Profiler',
 
 	),
 
