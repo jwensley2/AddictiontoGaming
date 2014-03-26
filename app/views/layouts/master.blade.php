5b3fc03101b79b8a2e7ed1dfcdccf3177e691d5f
Joseph Wensley <jwensley2@gmail.com>
2014-03-26 19:48:30 -0400

    update analytics code
    remove typekit and html5shiv

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -29,18 +29,14 @@
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
 	@if(App::environment() === 'production')
-		<!-- Google Analytics -->
-		<script type="text/javascript">
-			var _gaq = _gaq || [];
-			_gaq.push(['_setAccount', 'UA-9313553-1']);
-			_gaq.push(['_setDomainName', 'addictiontogaming.com']);
-			_gaq.push(['_trackPageview']);
+		<script>
+			(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+			(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+			m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+			})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
 
-			(function() {
-				var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-				ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-				var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-			})();
+			ga('create', 'UA-9313553-1', 'addictiontogaming.com');
+			ga('send', 'pageview');
 		</script>
 	@else
 		<script type="text/javascript" src="/assets/js/cssrefresh.js"></script>
