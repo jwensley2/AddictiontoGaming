e846c5b76dfca83de398c1d3a70de6ac847cacea
Joseph Wensley <jwensley2@gmail.com>
2014-04-04 18:20:22 -0400

    add a separate cache prefix for dev site

---
+++ app/config/develop/cache.php
@@ -0,0 +1,17 @@
+<?php
+
+return array(
+	/*
+	|--------------------------------------------------------------------------
+	| Cache Key Prefix
+	|--------------------------------------------------------------------------
+	|
+	| When utilizing a RAM based store such as APC or Memcached, there might
+	| be other applications utilizing the same cache. So, we'll specify a
+	| value to get prefixed to all our keys so we can avoid collisions.
+	|
+	*/
+
+	'prefix' => 'laravel_dev',
+
+);
