99875e0ccfd12799c4c6d230161e75e373099e7a
Joseph Wensley <jwensley2@gmail.com>
2014-04-08 19:42:54 -0400

    remove protocol from typekit url

--- app/views/layouts/master.blade.php
+++ app/views/layouts/master.blade.php
@@ -25,7 +25,7 @@
 	<![endif]-->
 
 	<!-- TypeKit Fonts -->
-	<script type="text/javascript" src="http://use.typekit.com/ove5wkp.js"></script>
+	<script type="text/javascript" src="//use.typekit.net/ove5wkp.js"></script>
 	<script type="text/javascript">try{Typekit.load();}catch(e){}</script>
 
 	@if(App::environment('production'))
