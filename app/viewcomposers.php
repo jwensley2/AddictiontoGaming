d8b2e1faacc402e9c6faaf4dd0c8a1274b292047
Joseph Wensley <jwensley2@gmail.com>
2014-04-08 20:24:27 -0400

    use donations repository

--- app/viewcomposers.php
+++ app/viewcomposers.php
@@ -1,23 +1,14 @@
 <?php
 
-use \Carbon\Carbon;
+use ATG\Repositories\Donation\EloquentDonation as Donations;
 
 View::composer('layouts.master', function($view)
 {
-	$settings = new Settings();
+	$repo = new Donations;
 
-	// Get the monthly cost
-	$donations['goal']  = $settings->get('MONTHLY_COST');
-
-	// When did this month start
-	$month_start = new Carbon('first day of this month', 'America/Winnipeg');
-	$month_start->setTime(0, 0, 0);
-
-	// Get the total donations for the month
-	$donations['total'] = Donation::where('created_at', '>=', $month_start)->where('status', 'completed')->sum('gross') ?: 0;
-
-	// Calculate the percentage of the goal that has been donated this month
-	$donations['percent'] = min(round((100 / $donations['goal']) * $donations['total'], 2), 100);
+	$donations['goal']    = $repo->monthlyGoal();
+	$donations['total']   = $repo->monthlyTotal();
+	$donations['percent'] = $repo->monthlyProgress();
 
 	$view->with('donations', $donations);
 });
